f1db2f56b3bb8d9bd4f9a6eb768b69d5888fa86a
Home <syaddad.snfa@gmail.com>
2022-12-19 19:02:27 +0700

    fix index.js startup

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
+const uuid = require('uuid');
+
+const config = require('./config/appconfig');
+const Logger = require('./utils/logger.js');
+
+const logger = new Logger();
+const app = express();
+
+app.set('config', config); // the system configrationsx
+app.use(bodyParser.json());
+app.use(require('method-override')());
+
+app.use(compression());
+app.use(cors());
+const swagger = require('./utils/swagger');
+
+
+process.on('SIGINT', () => {
+	logger.log('stopping the server', 'info');
+	process.exit();
+});
+
+app.set('db', require('./models/index.js'));
+
+app.set('port', process.env.DEV_APP_PORT);
+app.use('/api/docs', swagger.router);
+
+app.use((req, res, next) => {
+	req.identifier = uuid();
+	const logString = `a request has been made with the following uuid [${req.identifier}] ${req.url} ${req.headers['user-agent']} ${JSON.stringify(req.body)}`;
+	logger.log(logString, 'info');
+	next();
+});
+
+app.use(require('./router'));
+
+app.use((req, res, next) => {
+	logger.log('the url you are trying to reach is not hosted on our server T_T', 'error');
+	const err = new Error('Not Found');
+	err.status = 404;
+	res.status(err.status).json({ type: 'error', message: 'the url you are trying to reach is not hosted on our server' });
+
+	req.identifier = uuid();
+	const logString = `a request has been made with the following uuid [${req.identifier}] | ${req.ip} | ${req.url} | ${req.headers['user-agent']} ${JSON.stringify(req.body)}`;
+	const test = `test ${req.ip}  ${req.socket.remoteAddress}`;
+	logger.log(test , 'warn');
+	logger.log(logString, 'info');
+	next(err);
+});
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app)
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	const port = parseInt(val, 10);
+
+	if (Number.isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.DEV_APP_PORT || '3000');
+app.set('port', port);
+
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	const bind = typeof port === 'string'
+		? `Pipe ${port}`
+		: `Port ${port}`;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			logger.log(`${bind} requires elevated privileges`);
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			logger.log(`${bind} is already in use`);
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+	const addr = server.address();
+	const bind = typeof addr === 'string'
+		? `pipe ${addr}`
+		: `port ${addr.port}`;
+
+	logger.log(`the server started listining on port ${bind}`, 'info');
+}
+
+server.listen(port)
+server.on('error', onError);
+server.on('listening', onListening);
+
+// module.exports = app;
